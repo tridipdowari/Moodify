6660d78600acf3e852c440d3cb4b64e111863a98
tridipdowari <tridipdowari75@gmail.com>
2025-11-28 07:05:02 +0530

    first commit

---
+++ src/data/moods.js
@@ -0,0 +1,7 @@
+export const moods = [
+    { name: "Happy", color: "#FFD93D" },
+    { name: "Chill", color: "#6EC1E4" },
+    { name: "Focus", color: "#9B5DE5" },
+    { name: "Worksout", color: "#FF5F5f" },
+    { name: "Sad", color: "#6A6A6A" },
+];
---
+++ src/data/playlists.js
@@ -0,0 +1,19 @@
+export const playlists = {
+  Happy: [
+    { title: "Sunshine", artist: "Ava", duration: "2:43" },
+    { title: "Bright Day", artist: "Tom", duration: "3:12" }
+  ],
+  Chill: [
+    { title: "Calm Waves", artist: "Lofi Cat", duration: "2:54" },
+    { title: "Night Walk", artist: "Soft Rain", duration: "3:30" }
+  ],
+  Focus: [
+    { title: "Deep Focus", artist: "MindSync", duration: "4:02" }
+  ],
+  Workout: [
+    { title: "Power Up", artist: "BeastMode", duration: "3:18" }
+  ],
+  Sad: [
+    { title: "Rainy Window", artist: "Alone", duration: "3:50" }
+  ]
+};
